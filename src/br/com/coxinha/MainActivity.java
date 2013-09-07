83df02fb3ded8ab5bf2c4a0d6a6cfa177f2c3351
Rafael S. Suguiura <rafael.suguiura@gmail.com>
2013-09-07 19:00:38 -0300

    ListView created

--- src/br/com/coxinha/MainActivity.java
+++ src/br/com/coxinha/MainActivity.java
@@ -2,14 +2,23 @@ package br.com.coxinha;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
 
 public class MainActivity extends Activity
 {
+    private ArrayAdapter<String> adapter;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        adapter = new ArrayAdapter<String>(this, R.layout.list_order);
+
+        ListView listView = (ListView) findViewById(R.id.listview_of_orders);
+        listView.setAdapter(adapter);
     }
 }
