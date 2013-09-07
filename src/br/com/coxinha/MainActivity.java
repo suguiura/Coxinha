0cc86c43c25542d1480b9c7bc871ebaad4b17de8
Rafael S. Suguiura <rafael.suguiura@gmail.com>
2013-09-07 19:08:03 -0300

    Adding items to list

--- src/br/com/coxinha/MainActivity.java
+++ src/br/com/coxinha/MainActivity.java
@@ -1,13 +1,17 @@
 package br.com.coxinha;
 
+import java.util.LinkedList;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.ArrayAdapter;
 
 public class MainActivity extends Activity
 {
     private ArrayAdapter<String> adapter;
+    int counter;
 
     /** Called when the activity is first created. */
     @Override
@@ -20,5 +24,13 @@ public class MainActivity extends Activity
 
         ListView listView = (ListView) findViewById(R.id.listview_of_orders);
         listView.setAdapter(adapter);
+
+        counter = 0;
+    }
+
+    public void createOrder(View view) {
+        counter++;
+        adapter.add(getString(R.string.order_label) + " " + counter);
+        adapter.notifyDataSetChanged();
     }
 }
